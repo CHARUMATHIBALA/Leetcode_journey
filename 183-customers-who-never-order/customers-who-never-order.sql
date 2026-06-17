88055c2a3f9b9c9cae7e5d79b2a154b0532e8a4e
CHARUMATHIBALA <155135192+CHARUMATHIBALA@users.noreply.github.com>
2026-06-17 15:00:08 +0530

    Time: 629 ms (21.82%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,5 +1,2 @@
-SELECT c.name as Customers
- FROM Customers c
- LEFT JOIN Orders o
- ON c.id = o.customerId
- WHERE o.id IS NULL;
+
+select name as  Customers from Customers  where  id not in (select  customerId from Orders)
